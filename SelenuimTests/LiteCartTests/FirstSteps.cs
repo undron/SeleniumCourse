6f2cea511711731ecd345e9ffcf2328fa87c0fc4
Andrey <meetold@gmail.com>
2017-01-25 23:48:26 +0300

    Infrastructure added

---
+++ SelenuimTests/LiteCartTests/FirstSteps.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace LiteCartTests
+{
+    public class FirstSteps
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void JustOpenBrowserTest()
+        {
+            driver.Navigate().GoToUrl("http://www.tut.by");
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Close();
+        }
+    }
+}
