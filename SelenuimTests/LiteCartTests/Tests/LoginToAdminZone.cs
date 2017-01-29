a02b19cb9c79ff999ef2cc8c63df7f071fec72e8
Andrey <meetold@gmail.com>
2017-01-29 18:49:16 +0300

    Added test LoginToAdmin

---
+++ SelenuimTests/LiteCartTests/Tests/LoginToAdminZone.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace LiteCartTests.Tests
+{
+    class LoginToAdminZone
+    {
+        IWebDriver driver;
+
+        string siteUrl = "http://localhost/litecart/admin/";
+        string login = "admin";
+        string password = "admin";
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl(siteUrl);
+        }
+
+        [Test]
+        public void LoginToAdmin()
+        {
+            driver.FindElement(By.Name("username")).SendKeys(login);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+        }
+    }
+}
