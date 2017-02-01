e623e792c4f72ede7d8f2024ead5703729c4462c
Andrey <meetold@gmail.com>
2017-02-01 07:44:32 +0300

    Run tests in several browsers

--- SelenuimTests/LiteCartTests/Tests/LoginToAdminZone.cs
+++ SelenuimTests/LiteCartTests/Tests/LoginToAdminZone.cs
@@ -1,11 +1,16 @@
 ﻿using OpenQA.Selenium;
 using System;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Firefox;
 using NUnit.Framework;
 
 namespace LiteCartTests.Tests
 {
-    class LoginToAdminZone
+    [TestFixture(typeof(ChromeDriver))]
+    [TestFixture(typeof(InternetExplorerDriver))]
+    [TestFixture(typeof(FirefoxDriver))]
+    class LoginToAdminZone<TDriver> where TDriver : IWebDriver, new()
     {
         IWebDriver driver;
 
@@ -16,7 +21,51 @@ namespace LiteCartTests.Tests
         [SetUp]
         public void BeforeTest()
         {
-            driver = new ChromeDriver();
+            driver = new TDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl(siteUrl);
+        }
+
+        [Test]
+        public void LoginToAdmin()
+        {
+            driver.FindElement(By.Name("username")).SendKeys(login);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+        }
+    }
+
+    [TestFixture(true, @"c:\Program Files (x86)\Mozilla Firefox ESR\firefox.exe")]
+    [TestFixture(false, @"c:\Program Files\Nightly\firefox.exe")]
+    class LoginToAdminZoneInForefox
+    {
+        IWebDriver driver;
+
+        string siteUrl = "http://localhost/litecart/admin/";
+        string login = "admin";
+        string password = "admin";
+        bool legacy;
+        string binaryLocation;
+
+        public LoginToAdminZoneInForefox(bool legacy, string binaryLocation)
+        {
+            this.legacy = legacy;
+            this.binaryLocation = binaryLocation;
+        }
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            FirefoxOptions options = new FirefoxOptions();
+            options.UseLegacyImplementation = legacy;
+            options.BrowserExecutableLocation = binaryLocation;
+            driver = new FirefoxDriver(options);
             driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
             driver.Navigate().GoToUrl(siteUrl);
         }
