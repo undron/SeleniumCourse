b00c5b43c58154a186f340185309fc03906e28be
Andrey <meetold@gmail.com>
2017-02-06 21:29:52 +0300

    Case 7 added

---
+++ SelenuimTests/LiteCartTests/Tests/BaseTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCartTests.Tests
+{
+    public abstract class BaseTest
+    {
+        protected IWebDriver driver;
+
+        protected string siteUrl = "http://localhost/litecart/admin/";
+        protected string login = "admin";
+        protected string password = "admin";
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(10));
+            driver.Navigate().GoToUrl(siteUrl);
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ SelenuimTests/LiteCartTests/Tests/Case7AllAdminMenu.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCartTests.Tests
+{
+    class Case7AllAdminMenu : BaseTest
+    {
+        [SetUp]
+        public void LoginToAdminZone()
+        {
+            driver.FindElement(By.Name("username")).SendKeys(login);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.Name("login")).Click();
+        }
+
+        [Test]
+        public void MenuClickingTest()
+        {
+            int menuItemsCount = driver.FindElements(By.CssSelector("#box-apps-menu > li")).Count();
+            for (int i = 1; i <= menuItemsCount; i++)
+            {
+                var menuItem = driver.FindElement(By.CssSelector($"#box-apps-menu > li:nth-of-type({i})"));
+                menuItem.Click();
+
+                int subMenuItemsCount = driver.FindElements(By.CssSelector("#box-apps-menu > li.selected ul > li")).Count();
+                Assert.That(IsHeaderPresent(driver), Is.True, "Header not found!");
+
+                for (int j = 1; j <= subMenuItemsCount; j++)
+                {
+                    var subMenuItem = driver.FindElement(By.CssSelector($"#box-apps-menu > li.selected ul > li:nth-of-type({j})"));
+                    subMenuItem.Click();
+                    Assert.That(IsHeaderPresent(driver), Is.True, "Header not found!");
+                }
+            }
+        }
+
+        private bool IsHeaderPresent(IWebDriver driver)
+        {
+            return driver.FindElements(By.CssSelector("h1")).Count() > 0;
+        }
+    }
+}
