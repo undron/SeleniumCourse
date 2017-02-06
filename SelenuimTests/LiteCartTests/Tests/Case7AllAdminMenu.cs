0cf57ec13d40fa57570f5b384101e430bb25263a
Andrey <meetold@gmail.com>
2017-02-06 23:45:45 +0300

    Fixed admin URL

--- SelenuimTests/LiteCartTests/Tests/Case7AllAdminMenu.cs
+++ SelenuimTests/LiteCartTests/Tests/Case7AllAdminMenu.cs
@@ -13,7 +13,7 @@ namespace LiteCartTests.Tests
         [SetUp]
         public void LoginToAdminZone()
         {
-            driver.Navigate().GoToUrl(siteUrl);
+            driver.Navigate().GoToUrl(siteAdminUrl);
             driver.FindElement(By.Name("username")).SendKeys(login);
             driver.FindElement(By.Name("password")).SendKeys(password);
             driver.FindElement(By.Name("login")).Click();
