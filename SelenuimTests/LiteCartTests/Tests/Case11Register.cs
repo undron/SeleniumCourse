57af910b39bc964919b2131d5913bff288b688ec
Andrey <meetold@gmail.com>
2017-02-12 15:47:39 +0300

    Homework 11 + part of homework 12

---
+++ SelenuimTests/LiteCartTests/Tests/Case11Register.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCartTests.Tests
+{
+    public class Case11Register : BaseTest
+    {
+        [SetUp]
+        public void OpenProductsPage()
+        {
+            driver.Navigate().GoToUrl(Scenarios.siteUrl);
+        }
+
+        [Test]
+        public void RegisterUserTest()
+        {
+            string loginEmail = GenerateEmail();
+            string password = "password";
+
+            //Open registation page
+            driver.FindElement(By.CssSelector("#box-account-login a")).Click();
+
+            //Fill registration form and click 'Create Account'
+            driver.FindElement(By.Name("firstname")).SendKeys("Testname");
+            driver.FindElement(By.Name("lastname")).SendKeys("Testlastname");
+            driver.FindElement(By.Name("address1")).SendKeys("Somewhere, Some str., 1, 1");
+            driver.FindElement(By.Name("postcode")).SendKeys("12345");
+            driver.FindElement(By.Name("city")).SendKeys("Nowhere");
+            SetCountryByCode("US");
+            SetZoneByCode("AK");
+            driver.FindElement(By.Name("email")).SendKeys(loginEmail);
+            driver.FindElement(By.Name("phone")).SendKeys("+123456789");
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.Name("confirmed_password")).SendKeys(password);
+            driver.FindElement(By.Name("create_account")).Click();
+
+            //Logout
+            driver.FindElement(By.LinkText("Logout")).Click();
+
+            //Login
+            driver.FindElement(By.Name("email")).SendKeys(loginEmail);
+            driver.FindElement(By.Name("password")).SendKeys(password);
+            driver.FindElement(By.Name("login")).Click();
+
+            //Logout
+            driver.FindElement(By.LinkText("Logout")).Click();
+        }
+
+        private string GenerateEmail()
+        {
+            return DateTime.Now.Ticks + "@mail.com";
+        }
+
+        private void SetCountryByCode(string countryCode)
+        {
+            driver.FindElement(By.CssSelector(".select2-container")).Click();
+            driver.FindElement(By.CssSelector($".select2-results__options li[id$={countryCode}]")).Click();
+        }
+
+        private void SetZoneByCode(string zoneCode)
+        {
+            var zoneSelect = driver.FindElement((By.CssSelector("select[name='zone_code']")));
+            try
+            {
+                new WebDriverWait(driver, TimeSpan.FromSeconds(3))
+                    .Until(ExpectedConditions.ElementToBeClickable(zoneSelect));
+            }
+            catch (WebDriverTimeoutException)
+            {
+                TestContext.WriteLine("Zone select is not enabled. Has country any zones?");
+                return;
+            }
+            new SelectElement(zoneSelect).SelectByValue(zoneCode);
+        }
+    }
+}
---
+++ SelenuimTests/LiteCartTests/Tests/Case12AddProduct.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCartTests.Tests
+{
+    public class Case12AddProduct : BaseTest
+    {
+        [SetUp]
+        public void Login()
+        {
+            Scenarios.LoginToAdminZone(driver);
+        }
+
+        [Test]
+        public void AddProductTest()
+        {
+            string productName = "Test Duck";
+            string productCategory = "Rubber Ducks";
+            double productPrice = 7.99;
+
+            driver.FindElement(By.CssSelector("[href$='?app=catalog&doc=catalog']")).Click();
+            driver.FindElement(By.CssSelector("a[href$='edit_product']")).Click();
+
+            //General tab
+            driver.FindElement(By.CssSelector("[name='status'][value='1']")).Click();
+            driver.FindElement(By.Name("name[en]")).SendKeys(productName);
+            driver.FindElement(By.Name("code")).SendKeys("12345");
+            driver.FindElement(By.CssSelector($"[data-name='{productCategory}']")).Click();
+            new WebDriverWait(driver, TimeSpan.FromSeconds(3))
+                .Until(dr => dr.FindElements(By.CssSelector("[name='default_category_id'] > option")).Count == 2);
+            new SelectElement(driver.FindElement(By.Name("default_category_id"))).SelectByText(productCategory);
+
+            driver.FindElement(By.Name("new_images[]")).SendKeys(ImagesPath("RubberDuck.jpg"));
+        }
+
+        private string ImagesPath(string image)
+        {
+            string testDir = TestContext.CurrentContext.TestDirectory;
+            return System.IO.Path.Combine(testDir, "Images", image);
+        }
+    }
+}
