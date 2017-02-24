980d730d2065c333a1df8479671afef4e8b716d4
Andrey <meetold@gmail.com>
2017-02-24 18:36:37 +0300

    Homework (optional) 15, 16

---
+++ SelenuimTests/LiteCartTests/Tests/Case15Grid.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCartTests.Tests
+{
+    class Case15Grid
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void OpenProductsPage()
+        {
+            driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), DesiredCapabilities.Chrome());
+            driver.Navigate().GoToUrl(Scenarios.siteUrl);
+        }
+
+        [Test]
+        public void GridCheckStickersCountTest()
+        {
+            var productCards = driver.FindElements(By.CssSelector("li.product"));
+            foreach (var card in productCards)
+            {
+                var stickerCount = card.FindElements(By.CssSelector("div.sticker")).Count();
+                Assert.That(stickerCount, Is.EqualTo(1), "Wrong sticker count");
+            }
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver?.Quit();
+        }
+    }
+}
---
+++ SelenuimTests/LiteCartTests/Tests/Case16Browserstack.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCartTests.Tests
+{
+    class Case16Browserstack
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void OpenProductsPage()
+        {
+            DesiredCapabilities capability = new DesiredCapabilities();
+            capability.SetCapability("browser", "Chrome");
+            capability.SetCapability("browser_version", "56.0");
+            capability.SetCapability("os", "Windows");
+            capability.SetCapability("os_version", "7");
+            capability.SetCapability("resolution", "1024x768");
+
+            capability.SetCapability("browserstack.user", "");
+            capability.SetCapability("browserstack.key", "");
+            capability.SetCapability("browserstack.local", "true");
+
+            driver = new RemoteWebDriver(new Uri("http://hub-cloud.browserstack.com/wd/hub/"), capability);
+
+            driver.Navigate().GoToUrl(Scenarios.siteUrl);
+        }
+
+        [Test]
+        public void BrowserstackLoginToAdminZone()
+        {
+            Scenarios.LoginToAdminZone(driver);
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            driver?.Quit();
+        }
+    }
+}
