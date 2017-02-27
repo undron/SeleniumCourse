6b60780a3a12f747f9dcb85f67cf9c8580b17d26
Andrey <meetold@gmail.com>
2017-02-27 18:52:11 +0300

    Homework 17 added

---
+++ SelenuimTests/LiteCartTests/Tests/Case17BrowerLog.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteCartTests.Tests
+{
+    class Case17BrowerLog : BaseTest
+    {
+        [Test]
+        public void BrowserLogTest()
+        {
+            //Step 1
+            Scenarios.LoginToAdminZone(driver);
+
+            //Step 2
+            driver.Navigate().GoToUrl(Scenarios.siteAdminUrl + "?app=catalog&doc=catalog&category_id=1");
+
+            //Step 3
+            var productsLocator = By.CssSelector(".dataTable .row td:nth-of-type(3) a[href*='product_id']");
+            int productsCount = driver.FindElements(productsLocator).Count;
+            for (int i = 0; i < productsCount; i++)
+            {
+                driver.FindElements(productsLocator)[i].Click();
+                var logs = driver.Manage().Logs.GetLog("browser");
+
+                Assert.That(logs, Is.Empty);
+                driver.Navigate().Back();
+            }
+
+        }
+    }
+}
