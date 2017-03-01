33dc9bb6232c64dffd4626b6a5b9a5cc5aa0ac16
Andrey <meetold@gmail.com>
2017-03-01 18:28:22 +0300

    Added missed file to homework 19

---
+++ SelenuimTests/LiteCartTests/Framework/App/LiteCartApp.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace LiteCartTests.Framework
+{
+    public class LiteCartApp<TDriver> where TDriver : IWebDriver, new()
+    {
+        private AdminLoginPage adminLoginPage;
+        private StorePage storePage;
+        private ProductPage productPage;
+        private CartPage cartPage;
+
+        private IWebDriver driver;
+
+        public LiteCartApp()
+        {
+            driver = new TDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(1));
+            adminLoginPage = new AdminLoginPage(driver);
+            storePage = new StorePage(driver);
+            productPage = new ProductPage(driver);
+            cartPage = new CartPage(driver);
+        }
+
+        public void OpenAdminZone() => adminLoginPage.Open().LoginAsAdmin();
+
+        public StorePage OpenStorePage() => storePage.OpenStorePage();
+
+        public void AddPopularProductsToCart(int quantity)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                storePage.OpenFirstPopularProduct().AddToCart().OpenStorePage();
+            }
+        }
+
+        public void OpenCart() => storePage.OpenCartPage();
+
+        public void ClearCart() => cartPage.RemoveAllItems();
+
+        public void Quit()
+        {
+            driver?.Quit();
+        }
+    }
+}
